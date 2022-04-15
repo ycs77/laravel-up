3e86cc9d2ef4dae135bb9f5a29e8ab73651a76f9
Lucas Yang <yangchenshin77@gmail.com>
2022-04-15 15:22:28 +0800

    Fix copy bug

--- components/CodeBlock.vue
+++ components/CodeBlock.vue
@@ -2,11 +2,11 @@
   <div class="mt-8 flex border border-red-500 rounded-md">
     <pre
       class="flex-1 px-5 py-3 overflow-x-auto cursor-pointer"
-      @click="copy()"
+      @click="copyCode"
     >$ {{ content }}</pre>
 
     <div class="flex items-center px-1">
-      <button class="p-1" @click="copy()">
+      <button class="p-1" @click="copyCode">
         <HeroiconsOutlineDuplicate v-if="!copied" class="text-gray-400 w-6 h-6" />
         <HeroiconsOutlineCheckCircle v-else class="text-green-500 w-6 h-6" />
       </button>
@@ -26,7 +26,9 @@ const props = defineProps({
   },
 })
 
-const { copy, copied } = useClipboard({
-  source: props.content,
-})
+const { copy, copied } = useClipboard()
+
+const copyCode = () => {
+  copy(props.content)
+}
 </script>
