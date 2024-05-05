0fb0c814c8f1a9878c2827a543491801c968fad6
Lucas Yang <yangchenshin77@gmail.com>
2024-05-05 23:43:25 +0800

    Fix bash script confirm not working

--- server/routes/up/[range]/index.ts
+++ server/routes/up/[range]/index.ts
@@ -13,27 +13,35 @@ echo -e "\${RED}[This operation is destructive, please proceed with caution]\${N
 echo 'Please backup your git repository before executing this script'
 echo 'and clone the new repository to executing this script.'
 echo ''
-echo 'Is your git repository now ready to execute (y/n)? '
-read answer
+
+printf 'Is your git repository now ready to execute (y/N)? '
+set +e
+read yn < /dev/tty
+rc=$?
+set -e
+if [ $rc -ne 0 ]; then
+  exit 1
+fi
+if [ "$yn" != "y" ] && [ "$yn" != "yes" ]; then
+  exit 1
+fi
+
+# Pull Laravel commits...
+git remote add laravel https://github.com/laravel/laravel.git
+git fetch laravel
+git checkout ${startTag}
+git checkout -b laravel-upgrading-with-bash
+git merge --squash -Xtheirs --no-commit ${endTag}
+git restore --staged README.md CHANGELOG.md .styleci.yml .github/*
+git checkout -- README.md CHANGELOG.md .styleci.yml
+git clean -df
+
 echo ''
-
-if [ "$answer" != "\${answer#[Yy]}" ] ;then
-  # Pull Laravel commits...
-  git remote add laravel https://github.com/laravel/laravel.git
-  git fetch laravel
-  git checkout ${startTag}
-  git checkout -b laravel-upgrading-with-bash
-  git merge --squash -Xtheirs --no-commit ${endTag}
-  git restore --staged README.md CHANGELOG.md .styleci.yml .github/*
-  git checkout -- README.md CHANGELOG.md .styleci.yml
-  git clean -df
-
-  echo ''
-  echo -e "\${GREEN}*** Confirm upgrade changes ***\${NC}"
-  echo ''
-  echo 'Once confirmed complete run command to commit upgrade changes:'
-  echo ''
-  echo '  curl -s "${urlFromRequest(event)}/2" | bash'
-fi`
+echo ''
+echo -e "\${GREEN}*** Confirm upgrade changes ***\${NC}"
+echo ''
+echo 'Once confirmed complete run command to commit upgrade changes:'
+echo ''
+echo '  curl -s "${urlFromRequest(event)}/2" | bash'`
   )
 })
